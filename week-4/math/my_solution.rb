05e5282570284794816f51dd0f5e871533f290c9
bbensch09 <bbensch@gmail.com>
2015-10-20 00:29:48 -0700

    update for chal 4.3.2

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,25 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+num_1 = 10
+num_2 = 2
+
+def add(num_1, num_2)
+ return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ return num_1.to_f / num_2.to_f
+end

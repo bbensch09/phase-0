a8c4ae9185fb15f828cee6523b68ff116daf6d8a
bbensch09 <bbensch@gmail.com>
2015-10-20 00:20:20 -0700

    update chal 4.3.1

---
+++ week-4/variable-method.rb
@@ -0,0 +1,15 @@
+# puts "Enter your first name:"
+# first_name = gets.chomp
+
+# puts "Enter your middle name:"
+# middle_name = gets.chomp
+
+# puts "Enter your last name:"
+# last_name = gets.chomp
+
+# puts "It's nice to meet you " + first_name + " " + middle_name +  " " + last_name
+
+puts "What's your favorite number?"
+num = gets.chomp
+num_bigger = num.to_i + 1
+puts num_bigger.to_s + " is a bigger and better number"

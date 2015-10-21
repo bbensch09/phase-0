d21473ce088e8be3d2a018948464e937473182d6
bbensch09 <bbensch@gmail.com>
2015-10-21 11:26:18 -0700

    chal 4.6.1

--- week-4/smallest-integer/my_solution.rb
+++ week-4/smallest-integer/my_solution.rb
@@ -11,8 +11,31 @@
 # If +list_of_nums+ is empty the method should return nil
 
 # Your Solution Below
+
+#ITERATED VERSION
+      # def smallest_integer(list_of_nums)
+      #   sorted = false
+      #   until sorted == true
+      #     sorted = true
+      #     (list_of_nums.length - 1).times do |i|
+      #       #test to see if the current item is greater than the next item
+      #       if list_of_nums[i] > list_of_nums[i+1]
+      #         #if current is greater than next, switch their places, and consider sorting process incomplete (false); move to the next one.
+      #         list_of_nums[i], list_of_nums[i+1] = list_of_nums[i+1], list_of_nums [i]
+      #         sorted = false
+      #       end
+      #     end
+      #   end
+      #   return list_of_nums[0]
+      # end
+
+      # #test example
+      # #puts smallest_integer([1,2,3,4,-5])
+
+#REFACTORED VERSION
 def smallest_integer(list_of_nums)
-  # Your code goes here!
   list_of_nums.sort!
+  # ALT: this explicitly defines sort as ascending X to Y
+  #     list_of_nums.sort! {|x,y| x<=>y}
   return list_of_nums[0]
 end

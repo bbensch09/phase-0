3699d01f434742f3c126c531b4564cb339961689
bbensch09 <bbensch@gmail.com>
2015-11-04 14:58:18 -0800

    start chal 7.6

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,33 @@
+ // JavaScript Olympics
+
+// I paired by myself on this challenge. Start tiem: 4pm
+
+// This challenge took me [#] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+
+
+
+
+// Jumble your words
+
+
+
+
+// 2,4,6,8
+
+
+
+
+// "We built this city"
+
+
+
+
+// Reflection

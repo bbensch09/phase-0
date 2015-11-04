32a65fcd11ff7c47c1835f498b9d9e4413099dfb
bbensch09 <bbensch@gmail.com>
2015-11-04 14:56:39 -0800

    start chal 7.8

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,7 @@
+/*
+As a user, I have the two arrays above.
+Aa a user, I want to be able to calculate the sum of each array.
+As a user, I want to be able to claclulate the mean of each array.
+As a user, I want to be able to claclulate the median of each array.
+
+*/

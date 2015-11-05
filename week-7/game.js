60a29b8039d54069db32d26d3b3002fc6069d3f6
bbensch09 <bbensch@gmail.com>
2015-11-04 18:47:46 -0800

    start chal 7.7 design your own game

---
+++ week-7/game.js
@@ -0,0 +1,45 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description: The prisoner wants to guess the guards secret number, which is a number between 1 and 10. If he does, he'll be able to escape. With each guess, the guard will say higher, lower, or correct. After 3 guesses the guard chooses a new secret number. The prison has one chance to beg, which earns him a 4th guess. He can only beg once per game.
+// Overall mission: Escape from prison!
+// Goals: Guess the guard's password before it changes.
+// Characters: Guard, prisoner
+// Objects: Guard, Prisoner
+// Functions: Guess, Create Names, Beg (optional)
+
+// Pseudocode
+//create Guard object; has attributes of name and secret number, which contains a counter property
+//create Prisoner object; has attributes of name, status
+//create Guess function which takes an input of integer, compares it to the secret number and updates both the secret number's counter and the prisoners status
+//create Beg function, which allows the prisoner to speak his plea, and then enter one more guess
+//
+
+// Initial Code
+var Guard = {
+  name: "Guard";
+
+
+}
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//

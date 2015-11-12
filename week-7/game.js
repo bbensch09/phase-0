04a1df027e88d5749a38e9521bb98f49d02ae7fa
bbensch09 <bbensch@gmail.com>
2015-11-11 21:41:56 -0600

    chal 7.7 save progress

--- week-7/game.js
+++ week-7/game.js
@@ -56,40 +56,37 @@ console.log(Guard.secret_number);
 
 
 
-// SAMPLE CODE
+// BROWSER CONSOLE CODE --> THIS WORKS
 
-// var prompt = require('prompt');
-// prompt.start();
-
-// var randomNumber = getRandomNumber(10);
+// var secretNumber = getSecretNumber(10);
 // var guess;
 // var guessCount = 0;
 // var correctGuess = false;
 
-// function getRandomNumber( upper ) {
+// function getSecretNumber( upper ) {
 //   var num = Math.floor(Math.random() * upper) + 1;
 //   return num;
 // }
 
 // while (guessCount < 10) {
-//   guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+//   guess = prompt("If you can guess my secret number between 1 and 10, you'll be freed. What is it?");
 //   guessCount += 1;
-//   if (parseInt(guess) === randomNumber) {
+//   if (parseInt(guess) === secretNumber) {
 //     correctGuess = true;
 //     break;
 //   }
 // }
 
 // if (correctGuess) {
-//   console.log('You guessed the number!');
-//   console.log('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+//   console.log("Congrats, you're now free!");
+//   console.log('It took you ' + guessCount + ' tries to guess the number ' + secretNumber);
 // } else {
-//   console.log("Sorry,you did not guess the number");
+//   console.log("Sorry,you guessed wrong and are still captive.");
 // }
 
 
 
-// // Refactored Code
+// Refactored Code
 
 
 
--- week-7/test.js
+++ week-7/test.js
@@ -38,3 +38,28 @@ console.log(getRandomNumber(10));
   //   console.log(err);
   //   return 1;
   // }
+
+var secretNumber = getSecretNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+function getSecretNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
+}
+
+while (guessCount < 10) {
+  guess = prompt("If you can guess my secret number between 1 and 10, you'll be freed. What is it?");
+  guessCount += 1;
+  if (parseInt(guess) === secretNumber) {
+    correctGuess = true;
+    break;
+  }
+}
+
+if (correctGuess) {
+  console.log("Congrats, you're now free!");
+  console.log('It took you ' + guessCount + ' tries to guess the number ' + secretNumber);
+} else {
+  console.log("Sorry,you guessed wrong and are still captive.");

27b8aaee591bd577046cf6eb7faa86b575346277
bbensch09 <bbensch@gmail.com>
2015-11-24 14:12:11 -0800

    update chal 9.3.1 DOM manipulation

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,44 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+
+// Release 1:
+
+document.getElementById("release-0").classList.add("done");
+// document.getElementById('release-0').className = "done";
+
+// Release 2:
+
+document.getElementById('release-1').style.display = 'none';
+
+
+// Release 3:
+
+// document.getElementById('test').className = "done";
+document.getElementsByTagName('h1')[0].textContent = 'I completed release 3.';
+
+
+// Release 4:
+
+document.getElementById('release-3').style.backgroundColor = '#955251';
+
+
+// Release 5:
+
+var selected_elements = document.getElementsByClassName('release-4')
+
+for (var i = 0; i < selected_elements.length; i++) {
+  selected_elements[i].style.fontSize = '2em';
+};
+
+// Release 6:
+var tmpl = document.getElementById('hidden');
+document.body.appendChild(tmpl.content.cloneNode(true));
